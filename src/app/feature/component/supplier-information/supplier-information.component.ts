c79984db0e55c70e9f275566be80473b94815a9d
Ganta Sai Sirriisha <gantasaisirriishaas@clarium.tech>
2025-07-08 10:12:45 +0530

    changes

--- src/app/feature/component/supplier-information/supplier-information.component.ts
+++ src/app/feature/component/supplier-information/supplier-information.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed, effect, input } from '@angular/core';
-import {NgceComponentsModule , IFormConfig} from "@clarium/ngce-components"
-import {NgceIconModule} from '@clarium/ngce-icon'
+import { NgceComponentsModule, IFormConfig } from '@clarium/ngce-components';
+import { NgceIconModule } from '@clarium/ngce-icon';
 import { HeaderComponent } from '../../../shared/component/header/header.component';
 import { InvoiceData } from '../../model/invoice.model';
 import { InvoiceCardFormComponent } from '../../../shared/component/invoice-card-form/invoice-card-form.component';
@@ -9,13 +9,13 @@ import { InvoiceCardFormComponent } from '../../../shared/component/invoice-card
   selector: 'IOP-supplier-information',
   imports: [InvoiceCardFormComponent],
   templateUrl: './supplier-information.component.html',
-  styleUrl: './supplier-information.component.scss'
+  styleUrl: './supplier-information.component.scss',
 })
 export class SupplierInformationComponent {
-  title= 'Supplier Information';
-  formConfig !:IFormConfig;
+  title = 'Supplier Information';
+  formConfig!: IFormConfig;
   readonly SupplierData = input.required<InvoiceData>();
-  readonly data = computed(()=>this.SupplierData());
+  readonly data = computed(() => this.SupplierData());
   constructor() {
     effect(() => {
       const data = this.data();
@@ -24,60 +24,57 @@ export class SupplierInformationComponent {
       }
     });
   }
-  formInitialization(data : InvoiceData){
-    this.formConfig={
+  formInitialization(data: InvoiceData) {
+    this.formConfig = {
       layout: 'horizontal',
-      columns:2,
-      "customStyles": {
-        "padding": "20px",
-        "gap": "14px",
+      columns: 2,
+      customStyles: {
+        padding: '20px',
+        gap: '14px',
       },
-      "fields": [
+      fields: [
         {
-          "type": "text",
-          "name": "supplierName",
-          "label": "Supplier Name",
-          "value": this.data().supplier.suplier_name,
+          type: 'text',
+          name: 'supplierName',
+          label: 'Supplier Name',
+          value: this.data().supplier.supplier_name,
         },
         {
-          "type": "email",
-          "name": "supplier.supplier_address",
-          "label": "Supplier Address",
-          "value": this.data().supplier.supplier_address,
+          type: 'email',
+          name: 'supplier.supplier_address',
+          label: 'Supplier Address',
+          value: this.data().supplier.supplier_address,
         },
         {
-          "type": "text",
-          "name": "gstin",
-          "label": "Gstin",
-          "value": this.data().supplier.supplier_gst_no,
+          type: 'text',
+          name: 'gstin',
+          label: 'Gstin',
+          value: this.data().supplier.supplier_gst_no,
         },
         {
-          "type": "text",
-          "name": "msmeStatus",
-          "label": "Msme",
-          "value": this.data().supplier.msme_no
+          type: 'text',
+          name: 'msmeStatus',
+          label: 'Msme',
+          value: this.data().supplier.msme_no,
         },
         {
-          "type": "text",
-          "name": "supplier.pan_no",
-          "label": "Pan",
-          "value": this.data().supplier.pan_no,
+          type: 'text',
+          name: 'supplier.pan_no',
+          label: 'Pan',
+          value: this.data().supplier.pan_no,
         },
-        
       ],
-      "buttons": [
-      ]
-    }
+      buttons: [],
+    };
   }
 
-  Information={
+  Information = {
     width: '100%',
     border: 'none',
     'margin-top': '2rem',
     padding: '1.5rem',
-    'background-color':' #ffffff',
-    'border-radius':'0.5rem',
+    'background-color': ' #ffffff',
+    'border-radius': '0.5rem',
     'box-shadow': '0 0 0 1px rgba(0, 0, 0, 0.1)',
-  } 
-
+  };
 }
--- src/app/feature/store/invoice-store.service.ts
+++ src/app/feature/store/invoice-store.service.ts
@@ -4,74 +4,72 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { InvoiceData } from '../model/invoice.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InvoiceStoreService {
-  private readonly invoiceService= inject(InvoiceService);//
+  private readonly invoiceService = inject(InvoiceService); //
   EMPTY_INVOICE_DATA: InvoiceData = {
     invoice: {
-      invoice_number: "",
-      invoice_date: "",
-      irn_number: "",
-      acknowledgement_no: "",
-      acknowledgement_data: "",
-      e_way_bill_no: ""
+      invoice_number: '',
+      invoice_date: '',
+      irn_number: '',
+      acknowledgement_no: '',
+      acknowledgement_data: '',
+      e_way_bill_no: '',
     },
     supplier: {
-      suplier_name: "",
-      supplier_address: "",
-      supplier_gst_no: "",
-      msme_no: "",
-      pan_no: ""
+      supplier_name: '',
+      supplier_address: '',
+      supplier_gst_no: '',
+      msme_no: '',
+      pan_no: '',
     },
     purchase: {
-      order_no: "",
-      order_date: "",
-      transport_name: "",
-      agent_name: "",
-      LR_no: "",
-      LR_date: "",
-      merchandiser_name: ""
+      order_no: '',
+      order_date: '',
+      transport_name: '',
+      agent_name: '',
+      LR_no: '',
+      LR_date: '',
+      merchandiser_name: '',
     },
     taxes: {
-      taxable_value: "",
-      CGST_amount: "",
-      SGST_amount: "",
-      IGST_amount: "",
-      total_tax_amount: ""
+      taxable_value: '',
+      CGST_amount: '',
+      SGST_amount: '',
+      IGST_amount: '',
+      total_tax_amount: '',
     },
     discount: {
-      discount_percentage: "",
-      discount_amount: ""
+      discount_percentage: '',
+      discount_amount: '',
     },
     charges: {
-      other_deductions: "",
-      freight_charges: "",
-      other_charges: ""
+      other_deductions: '',
+      freight_charges: '',
+      other_charges: '',
     },
     amount: {
-      round_off_amount: "",
-      invoice_amount: "",
-      amount_in_words: ""
+      round_off_amount: '',
+      invoice_amount: '',
+      amount_in_words: '',
     },
     billing: {
-      billed_to: "",
-      bank_name: "",
-      bank_branch: "",
-      account_name: "",
-      account_no: "",
-      IFSC_code: ""
+      billed_to: '',
+      bank_name: '',
+      bank_branch: '',
+      account_name: '',
+      account_no: '',
+      IFSC_code: '',
     },
     product_details: {
       items: [],
       total_quantity: 0,
-      total_net_Amount: 0
-    }
+      total_net_Amount: 0,
+    },
   };
-  
-  
-  readonly invoiceDataStore= toSignal(this.invoiceService.getMockData(),{
-    initialValue: this.EMPTY_INVOICE_DATA
+
+  readonly invoiceDataStore = toSignal(this.invoiceService.getMockData(), {
+    initialValue: this.EMPTY_INVOICE_DATA,
   });
-  
 }
