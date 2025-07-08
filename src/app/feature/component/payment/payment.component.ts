09105e6bdf394686de7699bc752a15cd7d75fd2d
@VivekKumarSPS <vivekkumarsps@clarium.tech>
2025-07-08 11:13:55 +0530

    chage

--- src/app/feature/component/payment/payment.component.ts
+++ src/app/feature/component/payment/payment.component.ts
@@ -1,11 +1,11 @@
 import { Component, EffectRef, computed, effect, input } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import {NgceComponentsModule} from "@clarium/ngce-components"
 import {NgceIconModule} from "@clarium/ngce-icon"
 import { InvoiceData } from '../../model/invoice.model';
 @Component({
   selector: 'IOP-payment',
-  imports: [NgceComponentsModule,NgceIconModule],
+  imports: [NgceComponentsModule,NgceIconModule,ReactiveFormsModule],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.scss'
 })
@@ -13,45 +13,49 @@ export class PaymentComponent {
   readonly paymantData = input.required<InvoiceData>();
   readonly data = computed(()=>this.paymantData());
 
-  // readonly syncFormEffect: EffectRef = effect(() => {
-  //   const payment = this.data().purchase
+  readonly syncFormEffect: EffectRef = effect(() => {
+    const { discount, invoice, taxes, charges, amount } = this.data();
   
-  //   if (payment) {
-  //     this.form.get('invoice')?.patchValue({
-  //     });
-  //   }
-  // });
+    if (discount && invoice && taxes && charges && amount) {
+      this.form.get('invoice')?.patchValue({
+        invoice_amount: amount.invoice_amount ?? '',
+        round_off_amount: amount.round_off_amount,
+        amount_in_words: amount.amount_in_words,
+        freight_charges: charges.freight_charges,
+        other_deductions: charges.other_deductions,
+        other_charges: charges.other_charges,
+        discount_percentage: discount.discount_percentage,
+        discount_amount: discount.discount_amount,
+        CGST_amount: taxes.CGST_amount,
+        SGST_amount: taxes.SGST_amount,
+        IGST_amount: taxes.IGST_amount,
+        total_tax_amount: taxes.total_tax_amount,
+        taxable_value: taxes.taxable_value,
+        irn_number: invoice.irn_number ?? '',
+      });
+    }
+  });
+  
+  
   form = new FormGroup({
     invoice: new FormGroup({
-      // Section 1
-      totalPcs: new FormControl(60),
-      freightCharges: new FormControl(0),
-      masterDiscount: new FormControl(0),
-      cgst: new FormControl(6),
-      cgstRounded: new FormControl(0),
-      totalAmount: new FormControl({ value: 0, disabled: true }),
-
-      // Section 2
-      productTotal: new FormControl(25000.00),
-      miscAdditions: new FormControl(0),
-      specialDiscount: new FormControl(0),
-      sgst: new FormControl(6),
-      sgstRounded: new FormControl(0),
-
-      // Section 3
-      discount: new FormControl(0),
-      miscDeductions: new FormControl(0),
-      creditDays: new FormControl(15),
-      tcs: new FormControl(0),
-      tcsRounded: new FormControl(0),
-      irn: new FormControl('25abcd9f0d9f4a20f17b32f4sed5c0a7b7aa0078f9e7a2ed695'),
-
-      // Section 4
-      discRounded: new FormControl(0),
-      taxableValue: new FormControl(25000.00),
-      tradingNotes: new FormControl('-Select-')
+      invoice_amount: new FormControl(''),
+      freight_charges: new FormControl(''),
+      CGST_amount: new FormControl(''),
+      total_tax_amount: new FormControl(''),
+      discount_amount: new FormControl(''),
+      discount_percentage: new FormControl(''),
+      SGST_amount: new FormControl(''),
+      IGST_amount: new FormControl(''),
+      other_deductions: new FormControl(''),
+      other_charges: new FormControl(''),
+      amount_in_words: new FormControl(''),
+      irn_number: new FormControl(''),
+      round_off_amount: new FormControl(''),
+      taxable_value: new FormControl('')
     })
   });
+  
 
 
 
