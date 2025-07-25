887717c5aee38211c7edbbb2329ffa9d141ff1b3
Ganta Sai Sirriisha <gantasaisirriishaas@clarium.tech>
2025-07-25 13:57:49 +0530

    changes

--- src/app/feature/component/invoice-system-header/invoice-system-header.component.ts
+++ src/app/feature/component/invoice-system-header/invoice-system-header.component.ts
@@ -1,16 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { NgceComponentsModule } from '@clarium/ngce-components';
 
 @Component({
   selector: 'IOP-invoice-system-header',
-  imports: [RouterLink, NgceComponentsModule],
+  imports: [RouterLink, NgceComponentsModule, FormsModule],
   templateUrl: './invoice-system-header.component.html',
   styleUrl: './invoice-system-header.component.scss',
 })
 export class InvoiceSystemHeaderComponent {
   private readonly router = inject(Router);
+  layoutOption = output<string>();
+
   onUploadClick() {
     this.router.navigate(['upload']);
+  }
+
+  layoutOptions = ['Tabs', 'Scroll', 'Accordian'];
+
+  layoutOptionChanged(event: any) {
+    this.layoutOption.emit(event);
   }
 }
